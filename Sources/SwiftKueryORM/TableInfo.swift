17d4b33a7aa464d2aae0671e47c9fd8dc0039703
Brian Hirt <bhirt@me.com>
2019-03-12 19:39:46 -0600

    hack to allow models with defined identity columns to always use autoincrement.   this is right for me, but might not be right for you

--- Sources/SwiftKueryORM/TableInfo.swift
+++ Sources/SwiftKueryORM/TableInfo.swift
@@ -91,7 +91,7 @@ public class TableInfo {
                 }
                 if let SQLType = valueType as? SQLDataType.Type {
                     if key == idColumn.name && !idColumnIsSet {
-                        columns.append(Column(key, SQLType, primaryKey: true, notNull: !optionalBool))
+                        columns.append(Column(key, SQLType, autoIncrement: true, primaryKey: true, notNull: !optionalBool))
                         idColumnIsSet = true
                     } else {
                         columns.append(Column(key, SQLType, notNull: !optionalBool))
